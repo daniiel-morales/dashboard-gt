849ffecf2f3873454fa9df30be00447e99db1fa7
daniiel-morales <daniiel.morales@outlook.com>
2020-11-17 01:17:27 -0600

    add home page

---
+++ web/src/Ranking.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { CardBody } from 'reactstrap'
+import BarChart from './component/BarChart'
+import TableSort from './component/TableSort'
+
+class Ranking extends Component {
+  constructor () {
+    super()
+    this.state = {
+      loading: true
+    }
+  }
+
+  render () {
+    return (
+      <>
+        <div className='col-11 col-md-11 m-5'>
+          <div class='row'>
+            <div class='col-md-auto' />
+            <div class='col-3'>
+              <h1>  Top de Ministerios </h1>
+              <CardBody>
+                Porcentaje asignado del presupuesto de la nación para el año 2020
+              </CardBody>
+            </div>
+            <div class='col-8'>
+              <BarChart />
+              <TableSort />
+            </div>
+          </div>
+        </div>
+
+        <div className='col-11 col-md-11 m-5'>
+          <div class='row'>
+            <div class='col-md-auto' />
+            <div class='col-3'>
+              <h1>  ¿En qué afectó el COVID a la economía de estas instituciones? </h1>
+              <CardBody>
+                Ejecución de fondos por emergencia de COVID-19
+                <br />El Congreso aprobó la contratación de préstamos para atender la crisis sanitaria a cargo nueve ministerios, seis secretarías y tres dependencias del Estado. El monto asignado suma más de Q11 millardos
+              </CardBody>
+            </div>
+            <div class='col-8'>
+              <BarChart />
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
+}
+export default Ranking
