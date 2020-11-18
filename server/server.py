3a22d9ae1b2808eeff9dc7c84618821de4416f41
daniiel-morales <daniiel.morales@outlook.com>
2020-11-18 07:40:58 -0600

    add fetch data graph5

--- server/server.py
+++ server/server.py
@@ -31,10 +31,28 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
             opts = self.path.split('/')
 
-            if opts[2] == '2':
+            if opts[2] == '1':
+                for item in mycol.find({ '_id': entities[opts[1]]*10 + 1 }, { '_id': 0, opts[1]: 1}):
+                    db_dump.append(['Detalle', 'Egreso'])
+                    for i in range(6):
+                        db_dump.append([item[opts[1]][i]['detalle'], float(item[opts[1]][i]['egreso'])])
+            elif opts[2] == '2':
                 for item in mycol.find({ '_id': entities[opts[1]]*10 + 2 }, { '_id': 0, opts[1]: 1}):
                     for i in range(6):
-                        db_dump.append(item[opts[1]][i]['asignado'])
+                        db_dump.append(float(item[opts[1]][i]['asignado'])/1000)
+            elif opts[2] == '4':
+                for item in mycol.find({ '_id': str(entities[opts[1]]) + '-4-10' }, { '_id': 0, opts[1]: 1}):
+                    db_dump.append(['Detalle', 'Ingreso'])
+                    for i in range(6):
+                        db_dump.append([item[opts[1]][i]['detalle'], float(item[opts[1]][i]['ingreso'])])
+            if opts[2] == '5':
+                for item in mycol.find({ '_id': entities[opts[1]]*10 + 5 }, { '_id': 0, opts[1]: 1}):
+                    db_dump.append(['Location','Parent','Market trade volume (size)','Market increase/decrease (color)'])
+                    db_dump.append(['Proveedores', None, 0, 0])
+                    for i in range(6):
+                        db_dump.append([item[opts[1]][i]['proveedor'],'Proveedores', float(item[opts[1]][i]['debito']), -i*10])
+            else:
+                pass
                 
             self.wfile.write(toJSON(db_dump).encode('utf-8'))
 
