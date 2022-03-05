fa2631fdb57af318c76d158ade094ef4eb9899f2
daniiel-morales <daniiel.morales@outlook.com>
2022-03-04 23:21:04 -0600

    feat: chart component

---
+++ web/components/chart.tsx
@@ -0,0 +1,28 @@
+import { Chart as GChart } from 'react-google-charts'
+
+export enum chartType {
+    bar = 'ColumnChart',
+    table = 'Table',
+    pie = 'PieChart',
+}
+
+type Props = {
+    type: chartType
+    width?: string
+    data: string[]
+    options?: string[]
+}
+
+const Chart = ({ type, width, data, options }: Props) => {
+    return (
+        <GChart
+            chartType={type}
+            width={width}
+            height='inherit'
+            data={data}
+            options={options}
+        />
+    )
+}
+
+export default Chart
